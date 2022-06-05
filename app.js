1d3fdd93869f5a42a8d83dacb047149c7b89f84f
choyz1024 <nagatoyuki1024@gmail.com>
2022-06-05 23:41:32 +0800

    add session-base authentication

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const User = require('./models/User')
+var session = require('express-session')
 
 require('./config/mongoose')
 
@@ -12,13 +13,29 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(
+  session({
+    secret: 'vm,61j4j06kx7',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 1000 },
+  })
+)
 
 app.get('/', (req, res) => {
+  if (req.session.user) {
+    return res.render('login_success', {name : req.session.user})
+  }
   res.render('index')
 })
 
 app.post('/', (req, res) => {
   const { email, password } = req.body
+  if (email === '' || password === '') {
+    const msg = 'Please enter your email & password'
+    return res.render('index', { msg })
+  }
+
   User.findOne({
     $and: [{ email: email }, { password: password }],
   })
@@ -26,10 +43,11 @@ app.post('/', (req, res) => {
     .then((user) => {
       if (!user) {
         const msg = 'The email or password you entered is incorrect' 
-        res.render('index', { msg })
-        return
+        return res.render('index', { msg })
       }
-      res.render('login_success', { user })
+      const name = user.firstName
+      req.session.user = name
+      return res.render('login_success', { name })
     })
 })
 
